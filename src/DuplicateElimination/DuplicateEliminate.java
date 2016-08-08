a4ef752631ae81d90605758a36cf27ceb4b25563
Aravinth17 <aravinth.paranan@gmail.com>
2016-08-08 21:14:29 +0530

    Update DuplicateEliminate.java

--- src/DuplicateElimination/DuplicateEliminate.java
+++ src/DuplicateElimination/DuplicateEliminate.java
@@ -6,44 +6,42 @@ public class DuplicateEliminate {
 
 	public static void main(String[] args) {
 		int a[]=new int[5];
-		int b=0,c,counter=0;
+		int b=0,c,counter =0 ;
 		Scanner sc=new Scanner(System.in);
 		for(int i=0;i<5;i++)
 		{
 			System.out.println("Enter the number : ");
 			c=sc.nextInt();
+			
 			if(c>9&&c<101)
 			{
-			for(int j=0;j<i;j++)
-			{
-				if(a[j]==c)
+				for(int j=0;j<i;j++)
 				{
-					b=1;
-				}
+					if(a[j]==c)
+					{
+						b=1;
+					}
 				
-				else
+					else
+					{
+						continue;
+					}
+				}
+			
+				if(b > 0)
 				{
 					continue;
 				}
-			}
-			if(b > 0)
-			{
-				continue;
-			}
-			else
-			{
-				a[i]=c;
-				counter++;
-				for(int k=0;k<counter;k++)
-				System.out.println(a[k]);
-			}
-			}
-			else
-			{
-				System.out.println("Enter a number between 10 and 100 :");
-				i=i-1;
+				else
+				{
+					a[i]=c;
+					counter++;
 				
-			}
+					for(int k=0;k<counter;k++)
+					{
+						System.out.println(a[k]);
+					}
+				}
 			}
 		}
 
